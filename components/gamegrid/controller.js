6d3d04e99c330a8f9299e180031b9e6ff77bff25
Jack Schlotthauer <jack@manifestcommerce.com>
2015-10-10 02:54:43 -0700

    set depth to 2 for now seems to fix it

--- components/gamegrid/controller.js
+++ components/gamegrid/controller.js
@@ -8,6 +8,7 @@ angular
             'use strict';
 
             $scope.tells = [];
+            $scope.coordinates = [0,0];
 
             if (!userData.gameModuleSelected) {
                 $location.path( "/startscreen" );
@@ -15,26 +16,29 @@ angular
             }
 
             navigator.setDimensions(16,16);
+
+            // there's a wall-rendering bug when you set depth to 3 or more
             navigator.setDepth(2);
             navigator.init();
             navigator.updateNode(0,0, 0x01);
-//            navigator.updateNode(1,0, 0x01);
+            navigator.updateNode(1,0, 0x01);
             navigator.updateNode(1,1, 0x01);
 
-            var view = navigator.getView(0,0, 'east');
-//            navigator.debugMap(view);
-            navigator.debugView(view);
+            var view = navigator.getView($scope.coordinates[0],$scope.coordinates[1], 'east');
+
+            // navigator.debugMap(view);
+            // navigator.debugView(view);
 
             mazeRunner(view);
 
             $scope.map = {
                 zone: {
-                    name: "Party Select"
+                    name: "Level One"
                 },
                 location: {
                     coordinates: {
-                        x: 0,
-                        y: 0
+                        x: $scope.coordinates[0],
+                        y: $scope.coordinates[1]
                     },
                     compass: "n"
                 }
