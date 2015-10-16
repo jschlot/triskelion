a16da4c4a06d7692b5eac156b6800ce673fae368
Jack Schlotthauer <jack@manifestcommerce.com>
2015-10-16 12:44:42 -0700

    package files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,36 @@
 module.exports = function(grunt) {
+	'use strict';
 
 	require('time-grunt')(grunt);
 
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			files: ['Gruntfile.js', 'components/**/*.js'],
+			options: {
+				globals: {
+				jQuery: true
+				}
+			}
+		},		
+		watch: {
+			scripts: {
+				files: ['components/**/*.js'],
+				tasks: ['jshint'],
+				options: {
+				spawn: false,
+				},
+			}
+		}
 	});
+
+
 	
 	// Load the plugin that provides the "uglify" task.
-	// grunt.loadNpmTasks('grunt-contrib-uglify');
-	
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	// Default task(s).
-	grunt.registerTask('default', ['']);
+	grunt.registerTask('default', ['jshint']);
 
 };
