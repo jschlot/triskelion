d36fc2a60d2ead5822d84e4befec9c291b0a5025
Jack Schlotthauer <jackschlotthauer@jacks-imac.attlocal.net>
2015-10-19 14:07:51 -0700

    refactor services used in new character creation

--- wwwroot/components/character/service.js
+++ wwwroot/components/character/service.js
@@ -1,180 +1,175 @@
 /* global angular */
 angular
     .module('triskelion.character.service', ['triskelion.character.factory'])
-    .service('npcabilities',
+    .service('ability',
         function() {
             'use strict';
             var npcabilities = {
-                'dungeon': {
-                    swing: {
-                        name: "Swing",
-                        hotkey: "S",
-                        description: "",
-                        _self: "swing"
-                    },
-                    block: {
-                        name: "Block Attack",
-                        hotkey: "B",
-                        description: "",
-                        _self: "block"
-                    },
-                    taunt: {
-                        name: "Taunt Enemies",
-                        hotkey: "T",
-                        description: "",
-                        _self: "taunt"
-                    },
-                    fireball: {
-                        name: "Fireball Volley",
-                        hotkey: "F",
-                        description: "",
-                        _self: "fireball"
-                    },
-                    dragonblast: {
-                        name: "Dragon Blast",
-                        hotkey: "D",
-                        description: "",
-                        _self: "dragonblast"
-                    },
-                    inferno: {
-                        name: "Inferno Strike",
-                        hotkey: "I",
-                        description: "",
-                        _self: "inferno"
-                    },
-                    quickheal: {
-                        name: "Quick Heal",
-                        hotkey: "q",
-                        description: "",
-                        _self: "quickheal"
-                    },
-                    bubble: {
-                        name: "Bubble Shield",
-                        hotkey: "B",
-                        description: "",
-                        _self: "bubble"
-                    },
-                    radiate: {
-                        name: "Radiate Livelihood",
-                        hotkey: "R",
-                        description: "",
-                        _self: "radiate"
-                    },
-                    shoot: {
-                        name: "Shoot Arrow",
-                        hotkey: "S",
-                        description: "",
-                        _self: "shoot"
-                    },
-                    powershot: {
-                        name: "Power Shot",
-                        hotkey: "P",
-                        description: "",
-                        _self: "powershot"
-                    },
-                    flurry: {
-                        name: "Furry of Arrows",
-                        hotkey: "F",
-                        description: "",
-                        _self: "flurry"
-                    },
-                    pummel: {
-                        name: "Pummel",
-                        hotkey: "p",
-                        description: "",
-                        _self: "pummel"
-                    },
-                    whirlwind: {
-                        name: "Whirlwind",
-                        hotkey: "w",
-                        description: "",
-                        _self: "whirlwind"
-                    },
-                    smash: {
-                        name: "Smashing Blow",
-                        hotkey: "s",
-                        description: "",
-                        _self: "smash"
-                    }
+                swing: {
+                    name: "Swing",
+                    hotkey: "S",
+                    description: "",
+                    _self: "swing"
+                },
+                block: {
+                    name: "Block Attack",
+                    hotkey: "B",
+                    description: "",
+                    _self: "block"
+                },
+                taunt: {
+                    name: "Taunt Enemies",
+                    hotkey: "T",
+                    description: "",
+                    _self: "taunt"
+                },
+                fireball: {
+                    name: "Fireball Volley",
+                    hotkey: "F",
+                    description: "",
+                    _self: "fireball"
+                },
+                dragonblast: {
+                    name: "Dragon Blast",
+                    hotkey: "D",
+                    description: "",
+                    _self: "dragonblast"
+                },
+                inferno: {
+                    name: "Inferno Strike",
+                    hotkey: "I",
+                    description: "",
+                    _self: "inferno"
+                },
+                quickheal: {
+                    name: "Quick Heal",
+                    hotkey: "q",
+                    description: "",
+                    _self: "quickheal"
+                },
+                bubble: {
+                    name: "Bubble Shield",
+                    hotkey: "B",
+                    description: "",
+                    _self: "bubble"
+                },
+                radiate: {
+                    name: "Radiate Livelihood",
+                    hotkey: "R",
+                    description: "",
+                    _self: "radiate"
+                },
+                shoot: {
+                    name: "Shoot Arrow",
+                    hotkey: "S",
+                    description: "",
+                    _self: "shoot"
+                },
+                powershot: {
+                    name: "Power Shot",
+                    hotkey: "P",
+                    description: "",
+                    _self: "powershot"
+                },
+                flurry: {
+                    name: "Furry of Arrows",
+                    hotkey: "F",
+                    description: "",
+                    _self: "flurry"
+                },
+                pummel: {
+                    name: "Pummel",
+                    hotkey: "p",
+                    description: "",
+                    _self: "pummel"
+                },
+                whirlwind: {
+                    name: "Whirlwind",
+                    hotkey: "w",
+                    description: "",
+                    _self: "whirlwind"
+                },
+                smash: {
+                    name: "Smashing Blow",
+                    hotkey: "s",
+                    description: "",
+                    _self: "smash"
                 }
             };
 
             return npcabilities;
         }
     )
-    .service('npcraces',
+    .service('race',
         function() {
             'use strict';
             var npcraces = {
-                'dungeon': {
-                    human: {
-                        name: "Human",
-                        hotkey: "H",
-                        _self: "human"
-                    },
-                    elf: {
-                        name: "Elf",
-                        hotkey: "e",
-                        _self: "elf"
-                    },
-                    dwarf: {
-                        name: "Dwarf",
-                        hotkey: "D",
-                        _self: "dwarf"
-                    }
+                human: {
+                    name: "Human",
+                    hotkey: "H",
+                    _self: "human"
+                },
+                elf: {
+                    name: "Elf",
+                    hotkey: "e",
+                    _self: "elf"
+                },
+                dwarf: {
+                    name: "Dwarf",
+                    hotkey: "D",
+                    _self: "dwarf"
                 }
             };
 
             return npcraces;
         }
     )
-    .service('npctypes',
+    .service('classType',
         function() {
             'use strict';
             var npctypes = {
-                'dungeon': {
-                    support: {
-                        name: "Healer",
-                        hotkey: "H",
-                        _self: "support"
-                    },
-                    fighter: {
-                        name: "Fighter",
-                        hotkey: "F",
-                        _self: "fighter"
-                    },
-                    caster: {
-                        name: "Mage",
-                        hotkey: "M",
-                        _self: "caster"
-                    },
-                    thief: {
-                        name: "Thief",
-                        hotkey: "T",
-                        _self: "thief"
-                    },
-                    archer: {
-                        name: "Ranger",
-                        hotkey: "R",
-                        _self: "archer"
-                    }
+                support: {
+                    name: "Healer",
+                    hotkey: "H",
+                    _self: "support"
+                },
+                fighter: {
+                    name: "Fighter",
+                    hotkey: "F",
+                    _self: "fighter"
+                },
+                caster: {
+                    name: "Mage",
+                    hotkey: "M",
+                    _self: "caster"
+                },
+                thief: {
+                    name: "Thief",
+                    hotkey: "T",
+                    _self: "thief"
+                },
+                archer: {
+                    name: "Ranger",
+                    hotkey: "R",
+                    _self: "archer"
                 }
             };
 
             return npctypes;
         }
     )
-    .service('playerDB',['Character', 'npcabilities', 'npcraces', 'npctypes',
-        function(Character, npcabilities,npcraces,npctypes) {
+    .service('playerDB',['Character', 'ability', 'race', 'classType',
+        function(Character, ability, race, classType) {
             'use strict';
             var playerDB = {};
             playerDB.dungeon = [];
             
             var devonellah = new Character("Devonellah");
-            devonellah.character.abilities.small = { name: "flash heal" };
+                devonellah.character.abilities.small = ability.quickheal;
                         
             playerDB.dungeon.push(devonellah);
             
+            console.debug(playerDB.dungeon);
 
             return playerDB;
         }]
